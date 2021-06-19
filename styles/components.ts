9714b0e1779b77fd87f19f24898906726f3c99b5
mac <85849009+machooper@users.noreply.github.com>
2021-06-19 16:03:27 +0100

    Update components.ts

--- styles/components.ts
+++ styles/components.ts
@@ -1,9 +1,21 @@
 // * IMPORTANT: When starting a new project defined default button and other reusable component styles here
-
 export const Button = {
+  fonts: {
+    heading: "'system-ui', sans-serif",
+    body: "'system-ui', sans-serif"
+  },
   baseStyle: {
     fontWeight: "bold",
     textTransform: "uppercase",
     borderRadius: "base"
+  },
+  variants: {
+    solid: {
+      bg: "primary.900",
+      color: "gray.50",
+      _hover: {
+        bg: "primary.700"
+      }
+    }
   }
 }
