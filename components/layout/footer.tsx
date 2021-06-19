752f6ec4801604386b31e7855b069fd4af93c694
mac <85849009+machooper@users.noreply.github.com>
2021-06-19 16:41:06 +0100

    Create footer.tsx

---
+++ components/layout/footer.tsx
@@ -0,0 +1,20 @@
+import Logo from "./logo"
+import SocialIcons from "./social"
+import {Stack} from "@chakra-ui/layout"
+
+export default function Footer() {
+  return (
+    <Stack direction='column'>
+      <Stack
+        w='100vw'
+        direction='row'
+        justify='space-between'
+        align='center'
+        py={2}
+        px={4}>
+        <Logo />
+        <SocialIcons />
+      </Stack>
+    </Stack>
+  )
+}
