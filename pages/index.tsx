379aa7d3821f2853bd1381b3512fcca9b3730b7b
mac <85849009+machooper@users.noreply.github.com>
2021-06-19 16:05:41 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import {Button, useColorMode, useColorModeValue} from "@chakra-ui/react"
 export default function Home() {
-  const {colorMode, toggleColorMode} = useColorMode()
   return <h1>Welcome to next</h1>
 }
