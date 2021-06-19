f3f7fca10dcf797a349ed138ae1b3f06b02aeb2a
= <=>
2021-06-19 03:50:31 +0100

    test: :white_check_mark: Added tests

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  clearMocks: true,
+  moduleFileExtensions: ["js", "jsx", "ts", "tsx", "json", "node"],
+  roots: ["<rootDir>"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.tsx?$": "ts-jest"
+  }
+}
