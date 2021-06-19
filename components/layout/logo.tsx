3dd5c0512792cf94c27615c2869cf16a97a5f265
mac <85849009+machooper@users.noreply.github.com>
2021-06-19 16:42:13 +0100

    Update logo.tsx

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -1,10 +1,12 @@
 import {Img, Heading} from "@chakra-ui/react"
+import {siteTitle} from "pages/_app"
+
 
 export default function Logo() {
   return (
     <>
       <Img src='/logo.svg' alt='Todo Logo Image' boxSize={60} />
-      <Heading size='md'>Todo</Heading>
+      <Heading size='md'>{siteTitle}</Heading>
     </>
   )
 }
