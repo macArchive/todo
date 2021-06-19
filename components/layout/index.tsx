5bbd43031f840bf47763ce8f8776ad3678c70b2a
mac <85849009+machooper@users.noreply.github.com>
2021-06-19 16:11:00 +0100

    Update index.tsx

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,1 +1,20 @@
 
+import Head from "./head"
+import Header from "./header"
+import Content from "./content"
+import Footer from "./footer"
+
+interface LayoutProps {
+  title: string
+  description: string
+}
+export default function Layout({title, description}) {
+  return (
+    <>
+      <Head />
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  )
+}
