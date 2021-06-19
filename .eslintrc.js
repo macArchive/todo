81d614bf57a49e7868d983b1f710663b94c725ea
= <=>
2021-06-19 04:03:06 +0100

    eslintrc changed

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   settings: {
     react: {
-      version: 'detect'
+      version: "detect"
     }
   },
   env: {
@@ -9,20 +9,22 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: ['plugin:react/recommended', 'standard', 'eslint:recommended'],
-  parser: '@typescript-eslint/parser',
+  extends: ["plugin:react/recommended", "standard", "eslint:recommended"],
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaFeatures: {
       jsx: true
     },
     ecmaVersion: 12,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  plugins: ['react', '@typescript-eslint'],
-  ignorePatterns: ['**/node_modules', '**/out/*', '**/.next/*'],
+  plugins: ["react", "@typescript-eslint"],
+  ignorePatterns: ["**/node_modules", "**/out/*", "**/.next/*"],
   rules: {
-    'react/prop-types': 'off',
-    'react/react-in-jsx-scope': 'off',
-    'space-before-function-paren': 'off'
+    "react/prop-types": "off",
+    "react/react-in-jsx-scope": "off",
+    "space-before-function-paren": "off",
+    "object-curly-spacing": "off",
+    quotes: "off"
   }
 }
