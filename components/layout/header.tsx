91b9119514606923b55a4aa863ff376957235263
mac <85849009+machooper@users.noreply.github.com>
2021-06-21 00:00:30 +0100

    Update header.tsx

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -3,7 +3,7 @@ import {HStack, ButtonGroup, Button} from "@chakra-ui/react"
 
 export default function Header() {
   return (
-    <HStack w='100vw' justify='space-between' align='center' py={2} px={4}>
+    <HStack as='header' w='100vw' justify='space-between' align='center' py={2} px={4}>
       <Logo />
       <ButtonGroup spacing={2}>
         <Button variant='link'>Sign In</Button>
