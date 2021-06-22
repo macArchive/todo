f0489ead866bd4cb6a19dd23952b6fcb5df302c7
mac <85849009+machooper@users.noreply.github.com>
2021-06-22 23:25:07 +0100

    Update logo.tsx

--- components/layout/logo.tsx
+++ components/layout/logo.tsx
@@ -4,8 +4,8 @@ import {siteTitle} from 'pages/_app'
 
 export default function Logo() {
   return (
-    <Link href='/'>
-      <HStack spacing={1}>
+    <Link href='/' passHref>
+      <HStack spacing={1} cursor='pointer'>
         <Icon Name='logo' size='32px' />
         <Heading as='h1' size='md'>
           {siteTitle}
