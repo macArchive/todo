46dd58478d70706c6e1002432b950d713d98ea6e
mac <85849009+machooper@users.noreply.github.com>
2021-07-02 19:41:54 +0100

    Update head.tsx

--- components/layout/head.tsx
+++ components/layout/head.tsx
@@ -1,17 +1,17 @@
-import NextHead from "next/head";
-import { siteTitle } from "../../pages/_app";
+import NextHead from 'next/head'
+import { siteTitle } from '../../pages/_app'
 
 interface HeadProps {
-  pageTitle: string;
-  pageDescription: string;
-  socialImage: string;
-  socialImageText: string;
-  siteIcon: string;
-  touchIcon: string;
-  siteLocale: string;
-  siteType: "website" | "app";
-  siteUrl: string;
-  themeColor: string;
+  pageTitle: string
+  pageDescription: string
+  socialImage: string
+  socialImageText: string
+  siteIcon: string
+  touchIcon: string
+  siteLocale: string
+  siteType: 'website' | 'app'
+  siteUrl: string
+  themeColor: string
 }
 
 export default function Head({
@@ -28,27 +28,25 @@ export default function Head({
 }: HeadProps) {
   return (
     <NextHead>
-      <meta charSet="UTF-8"></meta>
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-      ></meta>
+      <meta charSet='UTF-8' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name='description' content={pageDescription} />
+      <meta property='og:title' content={`${pageTitle} | ${siteTitle}`} />
+      <meta property='og:description' content={pageDescription} />
+      <meta property='og:image' content={socialImage} />
+      <meta property='og:image:alt' content={socialImageText} />
+      <meta property='og:locale' content={siteLocale} />
+      <meta property='og:type' content={siteType} />
+      <meta property='og:url' content={siteUrl} />
+      <meta name='twitter:card' content={socialImage} />
+      <meta name='theme-color' content={themeColor} />
+      <link rel='canonical' href={siteUrl} />
+      <link rel='icon' href={siteIcon} type='image/svg+xml' />
+      <link rel='apple-touch-icon' href={touchIcon} />
+      <link rel='manifest' href='/manifest.json' />
       <title>
-          {pageTitle} | {siteTitle}
+        {pageTitle} | {siteTitle}
       </title>
-      <meta name="description" content={pageDescription}></meta>
-      <meta property="og:title" content={`${pageTitle} | ${siteTitle}`}></meta>
-      <meta property="og:description" content={pageDescription}></meta>
-      <meta property="og:image" content={socialImage}></meta>
-      <meta property="og:image:alt" content={socialImageText}></meta>
-      <meta property="og:locale" content={siteLocale}></meta>
-      <meta property="og:type" content={siteType}></meta>
-      <meta property="og:url" content={siteUrl}></meta>
-      <meta name="twitter:card" content={socialImage}></meta>
-      <link rel="canonical" href={siteUrl}></link>
-      <link rel="icon" href={siteIcon} type="image/svg+xml"></link>
-      <link rel="apple-touch-icon" href={touchIcon}></link>
-      <meta name="theme-color" content={themeColor}></meta>
     </NextHead>
-  );
+  )
 }
